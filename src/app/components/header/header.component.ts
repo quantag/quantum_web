fbc17a4ebe39a98a160d042bf54716edc6acac89
NikitaTudakov <tudakov.nikita@gmail.com>
2025-06-23 11:22:53 +0300

    added home link on logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NavComponent],
+  imports: [NavComponent, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
